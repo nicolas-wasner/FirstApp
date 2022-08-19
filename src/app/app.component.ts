485f7d6cd2ca19bcb6fddb89b7aa30826442e1e8
Nicolas Wasner <56253781+nicolas-wasner@users.noreply.github.com>
2022-08-19 10:12:56 +0200

    Commit v2
    Ajout des nouveaux composants

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,29 +7,43 @@ import {InstaSend} from "./models/insta-send-model";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  myInsta!: InstaSend;
-  myInsta1!: InstaSend;
-  myInsta2!: InstaSend;
+  myInstas!: InstaSend[];
+
 
   ngOnInit() {
-    this.myInsta = {
+    this.myInstas = [
+      {
       title: 'Smile',
       description: 'Une photo pour sourire',
       imageUrl: 'https://images.unsplash.com/photo-1659542216885-dcce5ca1de92?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80',
       like: 33,
       location: 'Aix-en-Provence'
-    };
-    this.myInsta1 = {
-      title: 'Smile',
-      description: 'Une photo pour sourire',
-      imageUrl: 'https://images.unsplash.com/photo-1659602172431-bf5552ede8db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
-      like: 109
-    };
-    this.myInsta2 = {
-      title: 'Smile',
-      description: 'Une photo pour sourire',
-      imageUrl: 'https://images.unsplash.com/photo-1657299143482-4f4ea1ebd71c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
-      like: 59
-    };
+    },
+      {
+        title: 'Smile',
+        description: 'Une photo pour sourire',
+        imageUrl: 'https://images.unsplash.com/photo-1659602172431-bf5552ede8db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+        like: 109
+      },
+      {
+        title: 'Smile',
+        description: 'Une photo pour sourire',
+        imageUrl: 'https://images.unsplash.com/photo-1657299143482-4f4ea1ebd71c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+        like: 59
+      },
+      {
+        title: 'Smile',
+        description: 'Une photo pour sourire',
+        imageUrl: 'https://images.unsplash.com/photo-1659542216885-dcce5ca1de92?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80',
+        like: 33,
+        location: 'Aix-en-Provence'
+      },
+      {
+        title: 'Smile',
+        description: 'Une photo pour sourire',
+        imageUrl: 'https://images.unsplash.com/photo-1659602172431-bf5552ede8db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+        like: 109
+      }
+      ]
   }
 }
